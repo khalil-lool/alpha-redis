242adb8d5e68940c0a9e2e7904f5b8f8ef5e74cd
kine2210 <fatoukinecamara1@esp.sn>
2026-07-04 13:50:39 +0000

    feat: interpréteur CLI - SET GET DEL LPUSH RPUSH LPOP RPOP

---
+++ src/cli/interpreter.h
@@ -0,0 +1,19 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+/* On inclut HashTable.h pour que interpreter.c
+   connaisse le type HashTable et ses fonctions */
+#include "../core/HashTable.h"
+
+/*
+ * execute_command :
+ * Reçoit la commande brute tapée par l'utilisateur (ex: "SET nom Fatou")
+ * et la base de données, exécute la commande, affiche le résultat.
+ *
+ * Paramètres :
+ *   - table   : la base de données HashTable
+ *   - command : la chaîne brute tapée par l'utilisateur
+ */
+void execute_command(HashTable *table, char *command);
+
+#endif /* INTERPRETER_H */
